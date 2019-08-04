4d4839d373fe3d079eccb510cb9955243a18cd22
Kenny van Vulpen <kennyvvulpen@ziggo.nl>
2019-08-04 20:55:06 +0200

    Commit missing

---
+++ src/SimpleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using SharpRakLib.Core.Client;
+using SharpRakLib.Protocol.RakNet;
+
+namespace SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RakNetClient client = new RakNetClient(new IPEndPoint(IPAddress.Loopback, 19132));
+            
+            Console.WriteLine("Started!");
+            Thread clientThread = new Thread(() =>
+            {
+                client.Start();
+            });
+            
+            clientThread.Start();
+            
+            while (!client.IsConnected)
+            {
+                UnconnectedPingOpenConnectionsPacket packet = new UnconnectedPingOpenConnectionsPacket();
+                packet.PingId = Stopwatch.GetTimestamp();
+                
+                client.Session.SendPacket(packet);
+                
+                Thread.Sleep(500);
+            }
+            
+            Console.WriteLine($"Finished!");
+        }
+    }
+}
